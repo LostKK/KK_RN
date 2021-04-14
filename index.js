2202ff3b0fb7b142494270af3ad21c6f69719a58
yongkang <1395175536@qq.com>
2021-04-14 18:23:56 +0800

    4.4

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import WelcomePage from './js/page/WelcomePage';
 import {name as appName} from './app.json';
+import AppNavigators from './js/navigator/AppNavigators'
 import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => WelcomePage);
