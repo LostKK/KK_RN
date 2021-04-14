1ace2958cd777ea98e4205f871c672cce9ad03b4
yongkang <1395175536@qq.com>
2021-04-14 17:20:43 +0800

    4.3

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import WelcomePage from './js/page/WelcomePage'
+import WelcomePage from './js/page/WelcomePage';
 import {name as appName} from './app.json';
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => WelcomePage);
--- js/page/WelcomePage.js
+++ js/page/WelcomePage.js
@@ -1,7 +1,19 @@
-import React, {Component, component} from 'react';
+import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 export default class WelcomePage extends Component {
+  componentDidMount() {
+    this.timer = setTimeout(() => {
+      //跳转到首页
+      // NavigationUtil.resetToHomePage(this.props);
+    }, 2000);
+  }
+
+  componentWillUnmount() {
+    //页面销毁时，清空计时器
+    this.timer && clearTimeout(this.timer);
+  }
+
   render() {
     return (
       <View style={styles.container}>
