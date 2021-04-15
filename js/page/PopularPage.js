57ad74bf0b1cb93a3861aab821d16734cfd7b32a
yongkang <1395175536@qq.com>
2021-04-15 10:35:29 +0800

    4.5

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
+import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
 export default class PopularPage extends Component {
+  render() {
+    const TabNavigator = createAppContainer(
+      createMaterialTopTabNavigator({
+        PopularTab1: {
+          screen: PopularTab,
+          navigationOptions: {
+            title: 'Tab1',
+          },
+        },
+        PopularTab2: {
+          screen: PopularTab,
+          navigationOptions: {
+            title: 'Tab2',
+          },
+        },
+      }),
+    );
+    return (
+      <View style={styles.container}>
+        <TabNavigator />
+      </View>
+    );
+  }
+}
+
+class PopularTab extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>PopularPage</Text>
+        <Text>PopularTab</Text>
       </View>
     );
   }
@@ -14,9 +41,7 @@ export default class PopularPage extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'skyblue',
+    marginTop: 30,
   },
   welcome: {
     fontSize: 20,
