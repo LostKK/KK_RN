797d4fcca9dbf4db222812d52b4f87474661df5b
yongkang <1395175536@qq.com>
2021-04-26 17:39:37 +0800

    8.8

--- js/page/DetailPage.js
+++ js/page/DetailPage.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import NavigationBar from '../common/NavigationBar';
+import ViewUtil from '../util/ViewUtil';
 
+const TRENDING_URL = 'https://github.com/';
+const THEME_COLOR = 'skyblue';
 export default class DetailPage extends Component {
+  constructor(props) {
+    super(props);
+    this.params = this.props.navigation.state.params;
+    const {projectModels} = this.params;
+    this.url = projectModels.html_url || TRENDING_URL + projectModels.fullName;
+    const title = projectModels.full_name || projectModels.fullName;
+    this.state = {
+      title: title,
+      url: this.url,
+      canGoBack: false,
+    };
+  }
+  onBack() {}
   render() {
-    const {navigation} = this.props;
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>DetailPage</Text>
-      </View>
+    let navigationBar = (
+      <NavigationBar
+        leftButton={ViewUtil.getLeftBackButton(() => this.onBack())}
+        title={this.state.title}
+        style={{backgroundColor: THEME_COLOR}}
+        rightButton={this.renderRightButton()}
+      />
     );
+    return <View style={styles.container}>{navigationBar}</View>;
   }
 }
 
